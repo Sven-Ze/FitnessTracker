47ff319e2ebc562e7654f95fb8076793c127ea16
Sven <svenzeindler@gmail.com>
2021-02-04 11:57:01 +0100

    Implemented Table View

--- app/src/main/java/com/example/fitnesstracker/UebersichtFragment.java
+++ app/src/main/java/com/example/fitnesstracker/UebersichtFragment.java
@@ -4,12 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class UebersichtFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_uebersicht, container, false);
+        View view = inflater.inflate(R.layout.fragment_uebersicht, container, false);
+        TableLayout tableLayout = (TableLayout) view.findViewById(R.id.tableLayout);
+
+        for (int i = 0; i < 5; i++) {
+            View tableRow = inflater.inflate(R.layout.table_item, null, false);
+            TextView uebung = (TextView) tableRow.findViewById(R.id.uebung);
+            TextView anzWiederholungen = (TextView) tableRow.findViewById(R.id.anzWiederholungen);
+            TextView gewicht = (TextView) tableRow.findViewById(R.id.gewicht);
+            TextView datum = (TextView) tableRow.findViewById(R.id.datum);
+
+            uebung.setText("Brust");
+            anzWiederholungen.setText("3");
+            gewicht.setText("55kg");
+            datum.setText("04-02-2021");
+            tableLayout.addView(tableRow);
+
+        }
+        return view;
     }
 }
